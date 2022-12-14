c09abb1825b2c852da660fbf592622157c5731e1
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2022-12-14 14:49:05 +0000

    applicationContext.xml file replaced by SportConfig.java with @Configuration annotation

---
+++ src/br/pessoal/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package br.pessoal.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.pessoal.springdemo")
+public class SportConfig {
+}
