8aafe6381af2a65fccc96e11ca56b9c159e54efe
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2022-12-14 10:03:39 +0000

    new main created to test @Scope annotation prototype, to be create a new bean every time instead singleton

--- src/br/pessoal/springdemo/TennisCoach.java
+++ src/br/pessoal/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package br.pessoal.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach{
 
     @Autowired
